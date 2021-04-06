3259b32c8511cdb6ea2f95c2badad8c1651b395e
Ryan <kennedy@osuosl.org>
2021-04-06 09:51:40 -0700

    Update Chef dependency to >= 16

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license          'Apache 2.0'
 description      'Installs/Configures the OpenStack Dashboard (Horizon)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '16.0.0'
+chef_version '>= 16.0'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
@@ -17,4 +18,3 @@ depends 'poise-python', '~> 1.5.1'
 
 issues_url 'https://launchpad.net/openstack-chef' if respond_to?(:issues_url)
 source_url 'https://github.com/openstack/cookbook-openstack-dashboard' if respond_to?(:source_url)
-chef_version '>= 12.5' if respond_to?(:chef_version)
